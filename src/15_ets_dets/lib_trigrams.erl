df2428bd2902a34b9668d2dd7c82fd9d3112a01a
simon-rock <simon29rock@gmail.com>
2014-06-27 17:54:46 +0800

    update note for erlang program

--- src/15_ets_dets/lib_trigrams.erl
+++ src/15_ets_dets/lib_trigrams.erl
@@ -1,7 +1,8 @@
 %%lib_trigrams.erl
 -module(lib_trigrams).
-%-export([start/0]).
-
+-export([timer_tests/0,
+         time_lookup_module_sets/0]).
+-import(lists, [reverse/1]).
 for_each_trigram_in_the_english_language(F, A0) ->
     {ok, Bin0} = file:read_file("354984si.ngl.gz"),
     Bin = zlib:gunzip(Bin0),
@@ -17,7 +18,7 @@ scan_word_list(L, F, A) ->
 %% scan the world looking for \r\n
 %% the second argument is the world (reversed) so it
 %% has to be reversed when we find \r\n or run out of characters
-get_next_word([$\r, $\n|T], L) -> {reverse({$\s|L}, T)};
+get_next_word([$\r,$\n|T], L) -> {reverse([$\s|L]), T};
 get_next_word([H|T], L)        -> get_next_word(T, [H|L]);
 get_next_word([], L)           -> {reverse([$\s|L]), []}.
 
@@ -37,3 +38,38 @@ make_a_set(Type, FileName) ->
     Size = ets:info(Tab, size),
     ets:delete(Tab),
     Size.
+
+make_mod_set() ->
+    D = sets:new(),
+    F = fun(Str, Set) ->
+                sets:add_element(list_to_binary(Str), Set) end,
+    D1 = for_each_trigram_in_the_english_language(F, D),
+    file:write_file("trigrams.set", [term_to_binary(D1)]).
+
+%% test get time for each search
+timer_tests() ->
+    time_lookup_ets_set("Ets ordered Set", "trigramsOS.tab"),
+    time_lookup_ets_set("Ets set", "trigramsS.tab"),
+    time_lookup_module_sets().
+
+time_lookup_ets_set(Type, File) ->
+    {ok, Tab} = ets:file2tab(File),
+    L = ets:tab2list(Tab),
+    Size = length(L),
+    {M, _} = timer:tc(?MODULE, lookup_all_ets, [Tab, L]),
+    io:foramt("~s lookup=~p micro seconds~n", [Type, M/Size]),
+    ets:delete(Tab).
+
+lookup_all_ets(Tab, L) ->
+    lists:foreach(fun({K}) -> ets:lookup(Tab, K) end, L).
+
+time_lookup_module_sets() ->
+    {ok, Bin} = file:read_file("trigrams.set"),
+    Set = binary_to_term(Bin),
+    Keys = sets:to_list(Set),
+    Size = length(Keys),
+    {M, _} = timer:tc(?MODULE, lookup_all_set, [Set, Keys]),
+    io:format("Module set lookup=~p micro seconds~n",[M/Size]).
+
+lookup_all_set(Set, L) ->
+    lists:foreach(fun(Key) -> sets:is_element(Key, Set) end, L).
